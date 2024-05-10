1de8a96cebf393def0cf791f560d53a4f6108d2a
tatxha <tatthamaharany@gmail.com>
2024-05-10 22:21:28 +0700

    fixed cart page

--- quiz3/lib/model/product.dart
+++ quiz3/lib/model/product.dart
@@ -5,6 +5,9 @@ class Product {
   final String description;
   final String category;
   final String image;
+  int quantity;
+  double totalPrice;
+  double totalOrders;
   // final String ratingRate;
   // final String ratingCount;
 
@@ -15,9 +18,12 @@ class Product {
     required this.description,
     required this.category,
     required this.image,
+    this.quantity = 1, 
+    this.totalOrders = 0.0
+    // this.totalPrice = 0.0,
     // required this.ratingRate,
     // required this.ratingCount,
-  });
+  }): totalPrice = double.parse(price);
 
   factory Product.fromJson(Map<String, dynamic> json) {
     final id = json['id'].toString();
@@ -26,6 +32,8 @@ class Product {
     final description = json['description'];
     final category = json['category'];
     final image = json['image'];
+    final quantity = json['quantity'] ?? 1; 
+    // final totalPrice = json['totalPrice'] ?? 0.0;
     // final ratingRate = json['rating']['rate'];
     // final ratingCount = json['rating']['count'];
     return Product(
@@ -35,6 +43,8 @@ class Product {
       description: description,
       category: category,
       image: image,
+      quantity: quantity,
+      // totalPrice: totalPrice,
       // ratingRate: ratingRate,
       // ratingCount: ratingCount,
     );
